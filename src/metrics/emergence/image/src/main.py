932c063f49db22a75d2281b868dcf07d6032c837
Mohammed Terry-Jack <mohammed@wluper.com>
2024-06-27 00:07:54 +0100

    tools added

--- src/metrics/emergence/image/src/main.py
+++ src/metrics/emergence/image/src/main.py
@@ -7,12 +7,12 @@ from eca import OneDimensionalElementaryCellularAutomata
 from transfer_entropy import pointwise_transfer_entropy, TransferEntropyNeighbour
 
 
-def display_ca(width:int) -> None:    
+def display_ca(width:int, time_steps:int, rule:int) -> None:    
     ca =  OneDimensionalElementaryCellularAutomata(
         lattice_width=width,
     )
-    for _ in range(500):
-        ca.transition(rule_number=110)
+    for _ in range(time_steps):
+        ca.transition(rule_number=rule)
 
     filtered_spacetime = pointwise_transfer_entropy(
         evolution=ca.evolution(), 
@@ -28,6 +28,21 @@ def display_ca(width:int) -> None:
             with ui.pyplot(figsize=(5, 10)):
                 imshow(filtered_spacetime)
             
+
+ui.label('width')
+width = ui.slider(
+    min=3, 
+    max=300, 
+    value=50, 
+    step=1, 
+).props('label-always')
+time_steps = ui.number(label='Time Steps', value=500, min=1, max=1000, step=1)
+rule = ui.number(label='Rule', value=110, min=0, max=256, step=1)
+ui.chip('Run', icon='ads_click', on_click=lambda: display_ca(
+    width=width.value,
+    time_steps=int(time_steps.value),
+    rule=int(rule.value)
+))
 emergence_window = ui.splitter()
-ui.slider(min=3, max=300, value=50, step=1, on_change=lambda width:display_ca(width=width.value))
+
 ui.run()
