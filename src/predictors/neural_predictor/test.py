22114fa4d77db3f5e67c9c08e094833854a8451e
Mohammed Terry-Jack <mohammed@wluper.com>
2024-10-06 01:06:13 +0100

    refactored

--- src/predictors/neural_predictor/test.py
+++ src/predictors/neural_predictor/test.py
@@ -1,25 +1,23 @@
 from numpy import concatenate, expand_dims
 from keras.datasets import fashion_mnist
-from keras.optimizers import Adam
 
 from vae import VAE
 from visualisation_utils import plot_latent_space, plot_label_clusters
 
-(x_train, y_train), (x_test, _) = fashion_mnist.load_data()
-fashion_mnist_data = concatenate([x_train, x_test], axis=0)
-fashion_mnist_data = expand_dims(fashion_mnist_data, -1).astype("float32") / 255
- 
 model = VAE(input_shape=(28, 28, 1), hidden_latent_dimension=2)
-print(model.encoder.summary())
-print(model.decoder.summary())
 
-model.compile(optimizer=Adam())
+
+(x_train, y_train), (x_test, _) = fashion_mnist.load_data()
+
+fashion_mnist_data = expand_dims(concatenate([x_train, x_test], axis=0), -1).astype("float32") / 255
 model.fit(fashion_mnist_data, epochs=1, batch_size=128)
 
 plot_latent_space(predictor=lambda sample:model.decoder.predict(sample, verbose=0))
  
-x_train = expand_dims(x_train, -1).astype("float32") / 255
-h_embedded, _, _ = model.encoder.predict(x_train)
+x_train_ = expand_dims(x_train, -1).astype("float32") / 255
+#print(x_train.shape) #(60000, 28, 28)
+#print(x_train_.shape) #(60000, 28, 28, 1)
+h_embedded, _, _ = model.encoder.predict(x_train_)
 
 plot_label_clusters(
     inputs_embedded=h_embedded,
--- src/predictors/neural_predictor/vae.py
+++ src/predictors/neural_predictor/vae.py
@@ -1,10 +1,11 @@
 
-from tensorflow import shape, reduce_sum, GradientTape, reduce_mean, square, exp
+from tensorflow import shape, reduce_sum, GradientTape, reduce_mean, square, exp, Tensor
 from tensorflow.random import normal
 from keras import Input, Model
 from keras.layers import Layer, Conv2D, Flatten, Dense, Reshape, Conv2DTranspose
 from keras.metrics import Mean
 from keras.losses import binary_crossentropy
+from keras.optimizers import Adam
 
 
 class Sampling(Layer):
@@ -20,58 +21,46 @@ class VAE(Model):
     def __init__(
         self, 
         input_shape:tuple[int,int,int], 
-        hidden_latent_dimension:int
+        hidden_latent_dimension:int,
     ) -> None:
+        
         super().__init__()
-
-    #TODO: continue refactoring from here...(make input 1D vector, make output 1D vector)
-        encoder_inputs = Input(shape=input_shape)
-        x = Conv2D(64, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-        x = Conv2D(128, 3, activation="relu", strides=2, padding="same")(x)
-        x = Flatten()(x)
-        x = Dense(16, activation="relu")(x)
-        mean = Dense(hidden_latent_dimension, name="mean")(x)
-        log_var = Dense(hidden_latent_dimension, name="log_var")(x)
-        z = Sampling()(mean, log_var)
-       
-        latent_inputs = Input(shape=(hidden_latent_dimension,))
-        x = Dense(7 * 7 * 64, activation="relu")(latent_inputs)
-        x = Reshape((7, 7, 64))(x)
-        x = Conv2DTranspose(128, 3, activation="relu", strides=2, padding="same")(x)
-        x = Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
-        decoder_outputs = Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
-
-        self.encoder = Model(encoder_inputs, [mean, log_var, z], name="encoder")
-        self.decoder = Model(latent_inputs, decoder_outputs, name="decoder")
+        
+        self.encoder = self.create_encoder(input_shape=input_shape, hidden_latent_dimension=hidden_latent_dimension)
+        self.decoder = self.create_decoder(hidden_latent_dimension=hidden_latent_dimension)
+        
         self.total_loss_tracker = Mean(name="total_loss")
-        self.reconstruction_loss_tracker = Mean(
-            name="reconstruction_loss"
-        )
+        self.reconstruction_loss_tracker = Mean(name="reconstruction_loss")
         self.kl_loss_tracker = Mean(name="kl_loss")
- 
+
+        self.compile(optimizer=Adam())
+
+        print(self.encoder.summary())
+        print(self.decoder.summary())
+
     @property
-    def metrics(self):
+    def metrics(self) -> list[Mean]:
         return [
             self.total_loss_tracker,
             self.reconstruction_loss_tracker,
             self.kl_loss_tracker,
         ]
- 
-    def train_step(self, data):
+
+    def train_step(self, train_data) -> dict[str,float]:
         with GradientTape() as tape:
-            mean,log_var, z = self.encoder(data)
-            reconstruction = self.decoder(z)
-            reconstruction_loss = reduce_mean(
-                reduce_sum(
-                    binary_crossentropy(data, reconstruction),
-                    axis=(1, 2),
-                )
+            mean,log_var, hidden_latent_layer = self.encoder(train_data)
+            train_data_reconstructed = self.decoder(hidden_latent_layer)
+            reconstruction_loss = self.calculate_reconstruction_loss(
+                y=train_data,
+                y_hat=train_data_reconstructed
+            ) 
+            kl_loss = self.calculate_kl_loss(
+                mean=mean,
+                log_var=log_var
             )
-            kl_loss = -0.5 * (1 + log_var - square(mean) - exp(log_var))
-            kl_loss = reduce_mean(reduce_sum(kl_loss, axis=1))
-            total_loss = reconstruction_loss + kl_loss
-        grads = tape.gradient(total_loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+            total_loss=reconstruction_loss+kl_loss
+        gradients = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
         self.total_loss_tracker.update_state(total_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
         self.kl_loss_tracker.update_state(kl_loss)
@@ -81,4 +70,40 @@ class VAE(Model):
             "kl_loss": self.kl_loss_tracker.result(),
         }
     
-   
+
+    @staticmethod
+    def create_decoder(hidden_latent_dimension:int) -> Model:
+        latent_inputs = Input(shape=(hidden_latent_dimension,))
+        layer1 = Dense(7 * 7 * 64, activation="relu")(latent_inputs)
+        layer2 = Reshape((7, 7, 64))(layer1)
+        layer3 = Conv2DTranspose(128, 3, activation="relu", strides=2, padding="same")(layer2)
+        layer4 = Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(layer3)
+        decoder_outputs = Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(layer4)
+        return Model(latent_inputs, decoder_outputs, name="decoder")
+
+    @staticmethod
+    def create_encoder(input_shape:tuple[int,int,int], hidden_latent_dimension:int) -> Model:
+        #TODO: (make input 1D vector, make output 1D vector)
+        encoder_inputs = Input(shape=input_shape) 
+        layer1 = Conv2D(64, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+        layer2 = Conv2D(128, 3, activation="relu", strides=2, padding="same")(layer1)
+        layer3 = Flatten()(layer2)
+        layer4 = Dense(16, activation="relu")(layer3)
+        mean = Dense(hidden_latent_dimension, name="mean")(layer4)
+        log_var = Dense(hidden_latent_dimension, name="log_var")(layer4)
+        layer_latent_hidden = Sampling()(mean=mean, log_var=log_var)
+        return Model(encoder_inputs, [mean, log_var, layer_latent_hidden], name="encoder")
+
+    @staticmethod
+    def calculate_reconstruction_loss(y:Tensor, y_hat:Tensor) -> Tensor:
+        return reduce_mean(
+            reduce_sum(
+                binary_crossentropy(y, y_hat),
+                axis=(1, 2),
+            )
+        )        
+
+    @staticmethod
+    def calculate_kl_loss(mean:Dense, log_var:Dense) -> Tensor:
+        kl_loss_ = -0.5 * (1 + log_var - square(mean) - exp(log_var))
+        return reduce_mean(reduce_sum(kl_loss_, axis=1)) 
