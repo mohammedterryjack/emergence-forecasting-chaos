76f84266f7e4a57e08c52316645869390fabf87f
Mohammed Terry-Jack <mohammed@wluper.com>
2024-11-14 22:08:48 +0000

    path

--- src/metrics/emergence/__init__.py
+++ src/metrics/emergence/__init__.py
@@ -7,4 +7,4 @@ from metrics.emergence.transfer_entropy import TransferEntropy
 from metrics.emergence.integrated_information import IntegratedInformation
 
 load_dotenv('config.env')
-startJVM(getenv("PATH_JAVA_VIRTUAL_MACHINE"), f"-Djava.class.path={getenv('PATH_JAR')}")
+startJVM(getenv("PATH_JAVA_VIRTUAL_MACHINE"), f"-Djava.class.path=src/metrics/emergence/infodynamics.jar")
