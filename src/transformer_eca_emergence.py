2a36ca097d8a6c0516c8dc8280b1db4f049ca7ae
Mohammed Terry-Jack <mohammed@wluper.com>
2024-11-17 22:53:20 +0000

    setting up transformer eca emergence

--- src/transformer_eca_emergence.py
+++ src/transformer_eca_emergence.py
@@ -12,7 +12,10 @@ from utils.data_loader import generate_dataset
 
 #--------Generate Train, Test Data----------
 
+batch_index_to_display = 0
 lattice_width=filtered_lattice_width=50
+emergence_context_length = 7
+transformer_context_length = 15
 forecast_length=100
 rule_number=3
 ics = [
@@ -24,34 +27,29 @@ batch_size = len(ics)
 n_epochs = 100
 emergence_filter_context_history= 7
 
-train_context_data, train_data = generate_dataset(
+train_context_data, train_test_data = generate_dataset(
     rule_number=rule_number,
     lattice_width=lattice_width,
     batch_size=batch_size,
-    context_sequence_length=0,
-    max_sequence_length=forecast_length,
+    context_sequence_length=transformer_context_length,
+    max_sequence_length=forecast_length * 2,
     initial_configurations=ics
 ) 
-train_data = array(train_data)
+train_data = [
+    sequence[:forecast_length]
+    for sequence in train_test_data
+]
 
-test_ics = [sequence[-1] for sequence in train_data]
-test_context_data, test_data = generate_dataset(
-    rule_number=rule_number,
-    lattice_width=lattice_width,
-    batch_size=batch_size,
-    context_sequence_length=0,
-    max_sequence_length=forecast_length,
-    initial_configurations=test_ics
-) 
-
-test_data_encoded = [
+train_test_data_encoded = [
     [
         eca_encoder(
             index=index, 
             array_size=lattice_width,
         ) for index in sequence
-     ] for sequence in test_data
+     ] for sequence in train_test_data
 ]
+
+
 
 #------Setup models--------
 
@@ -59,23 +57,45 @@ model_spacetime_only = Transformer(
     src_vocab_size= lattice_width, 
     tgt_vocab_size=lattice_width, 
     max_seq_length=forecast_length, 
-    src_encoder=eca_encoder,
-    tgt_encoder=eca_encoder,
+    src_encoder=lambda indexes,array_size:eca_encoder(
+        index=indexes[-1],
+        array_size=array_size
+    ),
+    tgt_encoder=lambda indexes,array_size:eca_encoder(
+        index=indexes[-1],
+        array_size=array_size
+    ),
 )
 model_spacetime_and_emergence = Transformer(
     src_vocab_size= lattice_width+filtered_lattice_width, 
     tgt_vocab_size=lattice_width, 
     max_seq_length=forecast_length, 
-    src_encoder=eca_encoder, #TODO: update this to sum(eca_and_emergence_encoder)
-    tgt_encoder=eca_encoder,
+    src_encoder=lambda indexes,array_size:sum(eca_and_emergence_encoder(
+        sequence=indexes,
+        array_size=array_size,
+        historical_context_length=emergence_context_length
+    )),
+    tgt_encoder=lambda indexes,array_size:sum(eca_and_emergence_encoder(
+        sequence=indexes,
+        array_size=array_size,
+        historical_context_length=emergence_context_length
+    )),
 )
 
 model_emergence_only = Transformer(
     src_vocab_size= lattice_width, 
     tgt_vocab_size=lattice_width, 
     max_seq_length=forecast_length, 
-    src_encoder=eca_encoder, #TODO: update this to eca_and_emergence_encoder[-1]
-    tgt_encoder=eca_encoder,
+    src_encoder=lambda indexes,array_size:eca_and_emergence_encoder(
+        sequence=indexes,
+        array_size=array_size,
+        historical_context_length=emergence_context_length
+    )[-1],
+    tgt_encoder=lambda indexes,array_size:eca_and_emergence_encoder(
+        sequence=indexes,
+        array_size=array_size,
+        historical_context_length=emergence_context_length
+    )[-1],
 )
 #--------Train models----------
 
@@ -109,7 +129,7 @@ predicted_spacetime_only = predict_n_encoded(
     source=test_context_data,
     target=train_data[:,:1],
     batch_size=batch_size,
-    forecast_horizon=forecast_length-1,
+    forecast_horizon=(forecast_length*2)-1,
     vector_to_index=lambda vector: eca_decoder(
         lattice=vector,
         binary_threshold=0.0
@@ -126,7 +146,7 @@ predicted_spacetime_only_encoded = array([
 ])
 
 
-
+#TODO: continue from here
 predicted_spacetime_and_emergence = predict_n_encoded(
     model=model_spacetime_and_emergence, 
     source=test_context_data,
@@ -175,20 +195,20 @@ predicted_emergence_only_encoded = array([
 #------Display results--------
 
 plot_results_with_emergence(
-    real=test_data_encoded[0],
-    predicted=predicted_spacetime_only_encoded[0],
+    real=train_test_data_encoded[batch_index_to_display],
+    predicted=predicted_spacetime_only_encoded[batch_index_to_display],
     emergence_filter=lambda vector:vector,
     lattice_width=lattice_width,
 )
 plot_results_with_emergence(
-    real=test_data_encoded[0],
-    predicted=predicted_spacetime_and_emergence_encoded[0],
+    real=train_test_data_encoded[batch_index_to_display],
+    predicted=predicted_spacetime_and_emergence_encoded[batch_index_to_display],
     emergence_filter=lambda vector:vector,
     lattice_width=lattice_width,
 )
 plot_results_with_emergence(
-    real=test_data_encoded[0],
-    predicted=predicted_emergence_only_encoded[0],
+    real=train_test_data_encoded[batch_index_to_display],
+    predicted=predicted_emergence_only_encoded[batch_index_to_display],
     emergence_filter=lambda vector:vector,
     lattice_width=lattice_width,
 )
